f859be952e1ee82a5ed9d388511ba8afe0f02088
Tejaswi Vutukuri <tejaswi.vutukuri@iths.com>
2022-06-11 09:26:59 +0530

    Update styling and little functioning

--- app.js
+++ app.js
@@ -11,17 +11,7 @@ app.use(express.static("public"));
 
 app.get("/", Controller.home);
 app.get("/myMessage/:id", Controller.mymessage);
-app.get("/test/:id", async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    await Message.findOne({ where: { id: id } }).then((msg) => {
-      if (!msg) return res.status(404).end();
-      res.render("test", { name: msg.name, message: msg.message });
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+
 app.use(errorHandler)
 
 app.listen(7000, () => console.log("Server running on port 7000"));
